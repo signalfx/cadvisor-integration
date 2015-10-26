aaaf226924a12e422d88e5f6e75a7e9974fc4ae7
Jack Lindamood <jack@signalfx.com>
2015-10-26 15:54:32 -0700

    Update vendor dir

---
+++ vendor/github.com/signalfx/metricproxy/protocol/protocol.go
@@ -0,0 +1,85 @@
+package protocol
+
+import (
+	"io"
+
+	"github.com/signalfx/metricproxy/dp/dpsink"
+	"github.com/signalfx/metricproxy/stats"
+)
+
+// Forwarder is the basic interface endpoints must support for the proxy to forward to them
+type Forwarder interface {
+	dpsink.Sink
+	stats.Keeper
+	io.Closer
+}
+
+// CompositeForwarder is a helper struct that expects users to inject their own versions of each
+// type
+type CompositeForwarder struct {
+	dpsink.Sink
+	stats.Keeper
+	io.Closer
+}
+
+var _ Forwarder = &CompositeForwarder{}
+
+// Listener is the basic interface anything that listens for new metrics must implement
+type Listener interface {
+	stats.Keeper
+	io.Closer
+}
+
+// ListenerDims are the common stat dimensions we expect on listener protocols
+func ListenerDims(name string, typ string) map[string]string {
+	return map[string]string{
+		"location": "listener",
+		"name":     name,
+		"type":     typ,
+	}
+}
+
+// ForwarderDims are the common stat dimensions we expect on forwarder protocols
+func ForwarderDims(name string, typ string) map[string]string {
+	return map[string]string{
+		"location": "forwarder",
+		"name":     name,
+		"type":     typ,
+	}
+}
+
+// CompositeListener is a helper struct that expects users to inject their own versions of each
+// type
+type CompositeListener struct {
+	stats.Keeper
+	io.Closer
+}
+
+var _ Listener = &CompositeListener{}
+
+type compositeCloser []io.Closer
+
+// CompositeCloser creates a new io.Closer whos Close() method calls close of each of the closers
+func CompositeCloser(closers ...io.Closer) io.Closer {
+	return compositeCloser(closers)
+}
+
+func (c compositeCloser) Close() error {
+	var e error
+	for _, closable := range c {
+		err := closable.Close()
+		if err != nil {
+			e = err
+		}
+	}
+	return e
+}
+
+// OkCloser allows any function to become a io.Closer that returns nil and calls itself on close
+type OkCloser func()
+
+// Close calls the wrapped function and returns nil
+func (o OkCloser) Close() error {
+	o()
+	return nil
+}
---
+++ vendor/github.com/signalfx/metricproxy/stats/keeper.go
@@ -0,0 +1,112 @@
+package stats
+
+import (
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/signalfx/golib/datapoint"
+	"github.com/signalfx/metricproxy/dp/dpsink"
+	"golang.org/x/net/context"
+)
+
+// A Keeper contains datapoints that describe its state and can be reported upstream
+type Keeper interface {
+	Stats() []*datapoint.Datapoint
+}
+
+type combinedStats []Keeper
+
+// CombineStats from multiple keepers in the order given as parameters.
+func (c combinedStats) Stats() []*datapoint.Datapoint {
+	ret := []*datapoint.Datapoint{}
+	for _, r := range c {
+		ret = append(ret, r.Stats()...)
+	}
+	return ret
+}
+
+// Combine multiple keepers into a single keeper
+func Combine(keepers ...Keeper) Keeper {
+	return combinedStats(keepers)
+}
+
+// A DimensionKeeper contains datapoints that describe its state and can be reported upstream
+type DimensionKeeper interface {
+	Stats(dimensions map[string]string) []*datapoint.Datapoint
+}
+
+type dimKeeper struct {
+	k    DimensionKeeper
+	dims map[string]string
+}
+
+// ToKeeper creates a keeper that pulls stats from k with dims
+func ToKeeper(k DimensionKeeper, dims map[string]string) Keeper {
+	return &dimKeeper{
+		k:    k,
+		dims: dims,
+	}
+}
+
+// ToKeeperMany creates a single keeper that pulls stats from each of keeps with dims
+func ToKeeperMany(dims map[string]string, keeps ...DimensionKeeper) Keeper {
+	ret := make([]Keeper, 0, len(keeps))
+	for _, k := range keeps {
+		ret = append(ret, ToKeeper(k, dims))
+	}
+	return Combine(ret...)
+}
+
+func (d *dimKeeper) Stats() []*datapoint.Datapoint {
+	return d.k.Stats(d.dims)
+}
+
+// StatDrainingThread will sleep delay between draining stats from each keeper and sending the full
+// stats to each sendTo sink
+type StatDrainingThread struct {
+	delay      time.Duration
+	sendTo     []dpsink.Sink
+	listenFrom []Keeper
+	ctx        context.Context
+}
+
+// NewDrainingThread returns a new DrainingThread to collect stats and send them to sinks
+func NewDrainingThread(delay time.Duration, sendTo []dpsink.Sink, listenFrom []Keeper, ctx context.Context) *StatDrainingThread {
+	ret := &StatDrainingThread{
+		delay:      delay,
+		sendTo:     sendTo,
+		listenFrom: listenFrom,
+		ctx:        ctx,
+	}
+	go ret.start()
+	return ret
+}
+
+// Stats returns all the stast this draining thread will report
+func (thread *StatDrainingThread) Stats() []*datapoint.Datapoint {
+	points := []*datapoint.Datapoint{}
+	for _, listenFrom := range thread.listenFrom {
+		log.WithField("listenFrom", listenFrom).Debug("Loading stats")
+		stats := listenFrom.Stats()
+		log.WithField("stats", stats).Debug("Stats loaded")
+		points = append(points, listenFrom.Stats()...)
+	}
+	return points
+}
+
+func (thread *StatDrainingThread) start() error {
+	log.WithField("listenFrom", thread.listenFrom).Info("Draining stats")
+	for {
+		select {
+		case <-thread.ctx.Done():
+			log.Debug("Request to stop stat thread")
+			return thread.ctx.Err()
+		case <-time.After(thread.delay):
+			points := thread.Stats()
+			for _, sendTo := range thread.sendTo {
+				// Note: Errors ignored
+				sendTo.AddDatapoints(thread.ctx, points)
+			}
+		}
+	}
+}
