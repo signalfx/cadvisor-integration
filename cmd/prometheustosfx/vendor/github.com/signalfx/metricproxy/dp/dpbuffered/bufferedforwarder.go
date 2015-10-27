b13d5cc3459e4a9a359c60229c01113f81522e7b
Jack Lindamood <jack@signalfx.com>
2015-10-27 08:56:52 -0700

    Add back vendor

---
+++ cmd/prometheustosfx/vendor/github.com/signalfx/metricproxy/dp/dpbuffered/bufferedforwarder.go
@@ -0,0 +1,232 @@
+package dpbuffered
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/signalfx/golib/datapoint"
+	"github.com/signalfx/golib/event"
+	"github.com/signalfx/metricproxy/config"
+	"github.com/signalfx/metricproxy/dp/dplocal"
+	"github.com/signalfx/metricproxy/dp/dpsink"
+	"golang.org/x/net/context"
+)
+
+// Config controls BufferedForwarder limits
+type Config struct {
+	BufferSize         int64
+	MaxTotalDatapoints int64
+	MaxTotalEvents     int64
+	MaxDrainSize       int64
+	NumDrainingThreads int64
+}
+
+// FromConfig loads the default config for a buffered forwarder from the proxy's config
+func (c *Config) FromConfig(conf *config.ForwardTo) *Config {
+	c.BufferSize = int64(*conf.BufferSize + 1)
+	c.MaxTotalDatapoints = int64(*conf.BufferSize)
+	c.MaxTotalEvents = int64(*conf.BufferSize)
+
+	c.NumDrainingThreads = int64(*conf.DrainingThreads)
+	c.MaxDrainSize = int64(*conf.MaxDrainSize)
+	return c
+}
+
+type stats struct {
+	totalDatapointsBuffered int64
+	totalEventsBuffered     int64
+}
+
+// BufferedForwarder abstracts out datapoint buffering.  Points put on its channel are buffered
+// and sent in large groups to a waiting sink
+type BufferedForwarder struct {
+	dpChan                      (chan []*datapoint.Datapoint)
+	eChan                       (chan []*event.Event)
+	config                      Config
+	stats                       stats
+	threadsWaitingToDie         sync.WaitGroup
+	blockingDrainWaitMutex      sync.Mutex
+	blockingEventDrainWaitMutex sync.Mutex
+
+	sendTo      dpsink.Sink
+	stopContext context.Context
+	stopFunc    context.CancelFunc
+}
+
+var _ dpsink.Sink = &BufferedForwarder{}
+
+// ErrDPBufferFull is returned by BufferedForwarder.AddDatapoints if the sink's buffer is full
+var ErrDPBufferFull = errors.New("unable to send more datapoints.  Buffer full")
+
+// AddDatapoints sends the datapoints to a chan buffer that eventually is flushed in big groups
+func (forwarder *BufferedForwarder) AddDatapoints(ctx context.Context, points []*datapoint.Datapoint) error {
+	atomic.AddInt64(&forwarder.stats.totalDatapointsBuffered, int64(len(points)))
+	if forwarder.config.MaxTotalDatapoints <= atomic.LoadInt64(&forwarder.stats.totalDatapointsBuffered) {
+		atomic.AddInt64(&forwarder.stats.totalDatapointsBuffered, int64(-len(points)))
+		return ErrDPBufferFull
+	}
+	select {
+	case forwarder.dpChan <- points:
+		return nil
+	case <-forwarder.stopContext.Done():
+		return forwarder.stopContext.Err()
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+// ErrEBufferFull is returned by BufferedForwarder.AddEvents if the sink's buffer is full
+var ErrEBufferFull = errors.New("unable to send more events.  Buffer full")
+
+// AddEvents sends the events to a chan buffer that eventually is flushed in big groups
+func (forwarder *BufferedForwarder) AddEvents(ctx context.Context, events []*event.Event) error {
+	atomic.AddInt64(&forwarder.stats.totalEventsBuffered, int64(len(events)))
+	if forwarder.config.MaxTotalEvents <= atomic.LoadInt64(&forwarder.stats.totalEventsBuffered) {
+		atomic.AddInt64(&forwarder.stats.totalEventsBuffered, int64(-len(events)))
+		return ErrEBufferFull
+	}
+	select {
+	case forwarder.eChan <- events:
+		return nil
+	case <-forwarder.stopContext.Done():
+		return forwarder.stopContext.Err()
+	case <-ctx.Done():
+		return ctx.Err()
+	}
+}
+
+// Stats related to this forwarder, including errors processing datapoints
+func (forwarder *BufferedForwarder) Stats(dimensions map[string]string) []*datapoint.Datapoint {
+	ret := make([]*datapoint.Datapoint, 0, 2)
+	ret = append(ret, dplocal.NewOnHostDatapointDimensions(
+		"datapoint_chan_backup_size",
+		datapoint.NewIntValue(int64(len(forwarder.dpChan))),
+		datapoint.Gauge,
+		dimensions))
+	ret = append(ret, dplocal.NewOnHostDatapointDimensions(
+		"event_chan_backup_size",
+		datapoint.NewIntValue(int64(len(forwarder.eChan))),
+		datapoint.Gauge,
+		dimensions))
+	ret = append(ret, dplocal.NewOnHostDatapointDimensions(
+		"datapoint_backup_size",
+		datapoint.NewIntValue(atomic.LoadInt64(&forwarder.stats.totalDatapointsBuffered)),
+		datapoint.Gauge,
+		dimensions))
+	ret = append(ret, dplocal.NewOnHostDatapointDimensions(
+		"event_backup_size",
+		datapoint.NewIntValue(atomic.LoadInt64(&forwarder.stats.totalEventsBuffered)),
+		datapoint.Gauge,
+		dimensions))
+	return ret
+}
+
+func (forwarder *BufferedForwarder) blockingDrainUpTo() []*datapoint.Datapoint {
+	// We block the mutex so we allow one drainer to fully drain until we use another
+	forwarder.blockingDrainWaitMutex.Lock()
+	defer forwarder.blockingDrainWaitMutex.Unlock()
+
+	// Block for at least one point
+	select {
+	case datapoints := <-forwarder.dpChan:
+	Loop:
+		for int64(len(datapoints)) < forwarder.config.MaxDrainSize {
+			select {
+			case datapoint := <-forwarder.dpChan:
+				datapoints = append(datapoints, datapoint...)
+				continue
+			default:
+				// Nothing left.  Flush this.
+				break Loop
+			}
+		}
+		atomic.AddInt64(&forwarder.stats.totalDatapointsBuffered, int64(-len(datapoints)))
+		return datapoints
+	case <-forwarder.stopContext.Done():
+		return []*datapoint.Datapoint{}
+	}
+}
+
+func (forwarder *BufferedForwarder) blockingDrainEventsUpTo() []*event.Event {
+	// We block the mutex so we allow one drainer to fully drain until we use another
+	forwarder.blockingEventDrainWaitMutex.Lock()
+	defer forwarder.blockingEventDrainWaitMutex.Unlock()
+
+	// Block for at least one event
+	select {
+	case events := <-forwarder.eChan:
+	Loop:
+		for int64(len(events)) < forwarder.config.MaxDrainSize {
+			select {
+			case event := <-forwarder.eChan:
+				events = append(events, event...)
+				continue
+			default:
+				// Nothing left.  Flush this.
+				break Loop
+			}
+		}
+		atomic.AddInt64(&forwarder.stats.totalEventsBuffered, int64(-len(events)))
+		return events
+	case <-forwarder.stopContext.Done():
+		return []*event.Event{}
+	}
+}
+
+// Close stops the threads that are flushing channel points to the next forwarder
+func (forwarder *BufferedForwarder) Close() {
+	forwarder.stopFunc()
+	forwarder.threadsWaitingToDie.Wait()
+}
+
+func (forwarder *BufferedForwarder) start() {
+	for i := int64(0); i < forwarder.config.NumDrainingThreads; i++ {
+		forwarder.threadsWaitingToDie.Add(1)
+		go func() {
+			defer forwarder.threadsWaitingToDie.Done()
+			for forwarder.stopContext.Err() == nil {
+				datapoints := forwarder.blockingDrainUpTo()
+				if len(datapoints) == 0 {
+					continue
+				}
+				err := forwarder.sendTo.AddDatapoints(forwarder.stopContext, datapoints)
+				if err != nil {
+					log.Error(fmt.Sprintf("Error sending datapoints %v", err))
+				}
+			}
+		}()
+		forwarder.threadsWaitingToDie.Add(1)
+		go func() {
+			defer forwarder.threadsWaitingToDie.Done()
+			for forwarder.stopContext.Err() == nil {
+				events := forwarder.blockingDrainEventsUpTo()
+				if len(events) == 0 {
+					continue
+				}
+				err := forwarder.sendTo.AddEvents(forwarder.stopContext, events)
+				if err != nil {
+					log.Error(fmt.Sprintf("Error sending events %v", err))
+				}
+			}
+		}()
+	}
+}
+
+// NewBufferedForwarder is used only by this package to create a forwarder that buffers its
+// datapoint channel
+func NewBufferedForwarder(ctx context.Context, config Config, sendTo dpsink.Sink) *BufferedForwarder {
+	context, cancel := context.WithCancel(ctx)
+	ret := &BufferedForwarder{
+		stopFunc:    cancel,
+		stopContext: context,
+		dpChan:      make(chan []*datapoint.Datapoint, config.BufferSize),
+		eChan:       make(chan []*event.Event, config.BufferSize),
+		config:      config,
+		sendTo:      sendTo,
+	}
+	ret.start()
+	return ret
+}
---
+++ cmd/prometheustosfx/vendor/github.com/signalfx/metricproxy/protocol/signalfx/signalfxlistener.go
@@ -0,0 +1,464 @@
+package signalfx
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"strings"
+	"sync"
+	"sync/atomic"
+	"time"
+
+	"errors"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/cep21/gohelpers/structdefaults"
+	"github.com/cep21/gohelpers/workarounds"
+	"github.com/golang/protobuf/proto"
+	"github.com/gorilla/mux"
+	"github.com/signalfx/com_signalfx_metrics_protobuf"
+	"github.com/signalfx/golib/datapoint"
+	"github.com/signalfx/golib/web"
+	"github.com/signalfx/metricproxy/config"
+	"github.com/signalfx/metricproxy/dp/dplocal"
+	"github.com/signalfx/metricproxy/dp/dpsink"
+	"github.com/signalfx/metricproxy/protocol"
+	"github.com/signalfx/metricproxy/protocol/collectd"
+	"github.com/signalfx/metricproxy/stats"
+	"golang.org/x/net/context"
+)
+
+// ListenerServer controls listening on a socket for SignalFx connections
+type ListenerServer struct {
+	stats.Keeper
+	name     string
+	listener net.Listener
+}
+
+var _ protocol.Listener = &ListenerServer{}
+
+// Close the exposed socket listening for new connections
+func (streamer *ListenerServer) Close() error {
+	return streamer.listener.Close()
+}
+
+// MericTypeGetter is an old metric interface that returns the type of a metric name
+type MericTypeGetter interface {
+	GetMetricTypeFromMap(metricName string) com_signalfx_metrics_protobuf.MetricType
+}
+
+// ConstTypeGetter always returns the wrapped metric type as a MericTypeGetter
+type ConstTypeGetter com_signalfx_metrics_protobuf.MetricType
+
+// GetMetricTypeFromMap returns the wrapped metric type object
+func (c ConstTypeGetter) GetMetricTypeFromMap(metricName string) com_signalfx_metrics_protobuf.MetricType {
+	return com_signalfx_metrics_protobuf.MetricType(c)
+}
+
+// ErrorReader are datapoint streamers that read from a HTTP request and return errors if
+// the stream is invalid
+type ErrorReader interface {
+	Read(ctx context.Context, req *http.Request) error
+}
+
+// ErrorTrackerHandler behaves like a http handler, but tracks error returns from a ErrorReader
+type ErrorTrackerHandler struct {
+	TotalErrors int64
+	reader      ErrorReader
+}
+
+// Stats returns the number of calls to AddDatapoint
+func (e *ErrorTrackerHandler) Stats(dimensions map[string]string) []*datapoint.Datapoint {
+	return []*datapoint.Datapoint{
+		dplocal.NewOnHostDatapointDimensions(
+			"total_errors",
+			datapoint.NewIntValue(e.TotalErrors),
+			datapoint.Counter,
+			dimensions),
+	}
+
+}
+
+// ServeHTTPC will serve the wrapped ErrorReader and return the error (if any) to rw if ErrorReader
+// fails
+func (e *ErrorTrackerHandler) ServeHTTPC(ctx context.Context, rw http.ResponseWriter, req *http.Request) {
+	if err := e.reader.Read(ctx, req); err != nil {
+		log.WithField("err", err).Debug("Bad request")
+		atomic.AddInt64(&e.TotalErrors, 1)
+		rw.WriteHeader(http.StatusBadRequest)
+		rw.Write([]byte(err.Error()))
+		return
+	}
+	rw.Write([]byte(`"OK"`))
+}
+
+// ProtobufDecoderV1 creates datapoints out of the V1 protobuf definition
+type ProtobufDecoderV1 struct {
+	Sink       dpsink.Sink
+	TypeGetter MericTypeGetter
+}
+
+var errInvalidProtobuf = errors.New("invalid protocol buffer sent")
+var errProtobufTooLarge = errors.New("protobuf structure too large")
+var errInvalidProtobufVarint = errors.New("invalid protobuf varint")
+
+func (decoder *ProtobufDecoderV1) Read(ctx context.Context, req *http.Request) error {
+	body := req.Body
+	bufferedBody := bufio.NewReaderSize(body, 32768)
+	for {
+		log.WithField("body", body).Debug("Starting protobuf loop")
+		buf, err := bufferedBody.Peek(1)
+		if err == io.EOF {
+			log.Debug("EOF")
+			return nil
+		}
+		buf, err = bufferedBody.Peek(4) // should be big enough for any varint
+
+		if err != nil {
+			log.WithField("err", err).Info("peek error")
+			return err
+		}
+		num, bytesRead := proto.DecodeVarint(buf)
+		if bytesRead == 0 {
+			// Invalid varint?
+			return errInvalidProtobufVarint
+		}
+		if num > 32768 {
+			// Sanity check
+			return errProtobufTooLarge
+		}
+		// Get the varint out
+		buf = make([]byte, bytesRead)
+		io.ReadFull(bufferedBody, buf)
+
+		// Get the structure out
+		buf = make([]byte, num)
+		_, err = io.ReadFull(bufferedBody, buf)
+		if err != nil {
+			return fmt.Errorf("unable to fully read protobuf message: %s", err)
+		}
+		var msg com_signalfx_metrics_protobuf.DataPoint
+		err = proto.Unmarshal(buf, &msg)
+		if err != nil {
+			return err
+		}
+		if datapointProtobufIsInvalidForV1(&msg) {
+			return errInvalidProtobuf
+		}
+		mt := decoder.TypeGetter.GetMetricTypeFromMap(msg.GetMetric())
+		if dp, err := NewProtobufDataPointWithType(&msg, mt); err == nil {
+			decoder.Sink.AddDatapoints(ctx, []*datapoint.Datapoint{dp})
+		}
+	}
+}
+
+func datapointProtobufIsInvalidForV1(msg *com_signalfx_metrics_protobuf.DataPoint) bool {
+	return msg.Metric == nil || msg.Value == nil
+}
+
+// JSONDecoderV1 creates datapoints out of the v1 JSON definition
+type JSONDecoderV1 struct {
+	TypeGetter MericTypeGetter
+	Sink       dpsink.Sink
+}
+
+func (decoder *JSONDecoderV1) Read(ctx context.Context, req *http.Request) error {
+	dec := json.NewDecoder(req.Body)
+	for {
+		var d JSONDatapointV1
+		if err := dec.Decode(&d); err == io.EOF {
+			break
+		} else if err != nil {
+			return err
+		} else {
+			log.WithField("dp", d).Debug("Got a new point")
+			if d.Metric == "" {
+				log.WithField("dp", d).Debug("Invalid Datapoint")
+				continue
+			}
+			mt := fromMT(decoder.TypeGetter.GetMetricTypeFromMap(d.Metric))
+			dp := datapoint.New(d.Metric, map[string]string{"sf_source": d.Source}, datapoint.NewFloatValue(d.Value), mt, time.Now())
+			decoder.Sink.AddDatapoints(ctx, []*datapoint.Datapoint{dp})
+		}
+	}
+	return nil
+}
+
+// ProtobufDecoderV2 decodes protocol buffers in signalfx's v2 format and sends them to Sink
+type ProtobufDecoderV2 struct {
+	Sink dpsink.Sink
+}
+
+var errInvalidContentLength = errors.New("invalid Content-Length")
+
+func (decoder *ProtobufDecoderV2) Read(ctx context.Context, req *http.Request) error {
+	if req.ContentLength == -1 {
+		return errInvalidContentLength
+	}
+
+	// TODO: Source of memory creation.  Maybe pass buf in?
+	buf := make([]byte, req.ContentLength)
+	readLen, err := io.ReadFull(req.Body, buf)
+	if err != nil {
+		log.WithField("err", err).WithField("len", readLen).WithField("content-len", req.ContentLength).Warn("Unable to fully read from buffer")
+		return err
+	}
+	var msg com_signalfx_metrics_protobuf.DataPointUploadMessage
+	err = proto.Unmarshal(buf, &msg)
+	if err != nil {
+		log.Debug("Unable to unmarshal")
+		return err
+	}
+	dps := make([]*datapoint.Datapoint, 0, len(msg.GetDatapoints()))
+	for _, protoDb := range msg.GetDatapoints() {
+		if dp, err := NewProtobufDataPointWithType(protoDb, com_signalfx_metrics_protobuf.MetricType_GAUGE); err == nil {
+			dps = append(dps, dp)
+		}
+	}
+	return decoder.Sink.AddDatapoints(ctx, dps)
+}
+
+// JSONDecoderV2 decodes v2 json data for signalfx and sends it to Sink
+type JSONDecoderV2 struct {
+	Sink dpsink.Sink
+}
+
+func (decoder *JSONDecoderV2) Read(ctx context.Context, req *http.Request) error {
+	dec := json.NewDecoder(req.Body)
+	var d JSONDatapointV2
+	if err := dec.Decode(&d); err != nil {
+		return err
+	}
+	dps := make([]*datapoint.Datapoint, 0, len(d))
+	for metricType, datapoints := range d {
+		mt, ok := com_signalfx_metrics_protobuf.MetricType_value[strings.ToUpper(metricType)]
+		if !ok {
+			log.WithField("metricType", metricType).Warn("Unknown metric type")
+			continue
+		}
+		for _, jsonDatapoint := range datapoints {
+			v, err := ValueToValue(jsonDatapoint.Value)
+			if err != nil {
+				log.WithField("err", err).Warn("Unable to get value for datapoint")
+			} else {
+				dp := datapoint.New(jsonDatapoint.Metric, jsonDatapoint.Dimensions, v, fromMT(com_signalfx_metrics_protobuf.MetricType(mt)), fromTs(jsonDatapoint.Timestamp))
+				dps = append(dps, dp)
+			}
+		}
+	}
+	return decoder.Sink.AddDatapoints(ctx, dps)
+}
+
+var defaultConfig = &config.ListenFrom{
+	ListenAddr:      workarounds.GolangDoesnotAllowPointerToStringLiteral("127.0.0.1:12345"),
+	TimeoutDuration: workarounds.GolangDoesnotAllowPointerToTimeLiteral(time.Second * 30),
+	Name:            workarounds.GolangDoesnotAllowPointerToStringLiteral("signalfxlistener"),
+	JSONEngine:      workarounds.GolangDoesnotAllowPointerToStringLiteral("native"),
+}
+
+// ListenerLoader loads a listener for signalfx protocol from config
+func ListenerLoader(ctx context.Context, sink dpsink.Sink, listenFrom *config.ListenFrom) (*ListenerServer, error) {
+	structdefaults.FillDefaultFrom(listenFrom, defaultConfig)
+
+	log.WithField("listenFrom", listenFrom).Info("Creating signalfx listener using final config")
+	return StartServingHTTPOnPort(ctx, sink, *listenFrom.ListenAddr, *listenFrom.TimeoutDuration, *listenFrom.Name)
+}
+
+type jsonMarshalStub func(v interface{}) ([]byte, error)
+
+type metricHandler struct {
+	metricCreationsMapMutex sync.Mutex
+	metricCreationsMap      map[string]com_signalfx_metrics_protobuf.MetricType
+	jsonMarshal             jsonMarshalStub
+}
+
+func (handler *metricHandler) ServeHTTP(writter http.ResponseWriter, req *http.Request) {
+	dec := json.NewDecoder(req.Body)
+	var d []MetricCreationStruct
+	if err := dec.Decode(&d); err != nil {
+		log.WithField("err", err).Info("Invalid metric creation request")
+		writter.WriteHeader(http.StatusBadRequest)
+		writter.Write([]byte(`{msg:"Invalid creation request"}`))
+		return
+	}
+	log.WithField("d", d).Debug("Got metric types")
+	handler.metricCreationsMapMutex.Lock()
+	defer handler.metricCreationsMapMutex.Unlock()
+	ret := []MetricCreationResponse{}
+	for _, m := range d {
+		metricType, ok := com_signalfx_metrics_protobuf.MetricType_value[m.MetricType]
+		if !ok {
+			writter.WriteHeader(http.StatusBadRequest)
+			writter.Write([]byte(`{msg:"Invalid metric type"}`))
+			return
+		}
+		handler.metricCreationsMap[m.MetricName] = com_signalfx_metrics_protobuf.MetricType(metricType)
+		ret = append(ret, MetricCreationResponse{Code: 409})
+	}
+	unmarshal := handler.jsonMarshal
+	if unmarshal == nil {
+		unmarshal = json.Marshal
+	}
+	toWrite, err := unmarshal(ret)
+	if err != nil {
+		log.WithField("err", err).Warn("Unable to marshal json")
+		writter.WriteHeader(http.StatusBadRequest)
+		writter.Write([]byte(`{msg:"Unable to marshal json!"}`))
+		return
+	}
+	writter.WriteHeader(http.StatusOK)
+	writter.Write([]byte(toWrite))
+}
+
+func (handler *metricHandler) GetMetricTypeFromMap(metricName string) com_signalfx_metrics_protobuf.MetricType {
+	handler.metricCreationsMapMutex.Lock()
+	defer handler.metricCreationsMapMutex.Unlock()
+	mt, ok := handler.metricCreationsMap[metricName]
+	if !ok {
+		return com_signalfx_metrics_protobuf.MetricType_GAUGE
+	}
+	return mt
+}
+
+// StartServingHTTPOnPort servers http requests for Signalfx datapoints
+func StartServingHTTPOnPort(ctx context.Context, sink dpsink.Sink, listenAddr string,
+	clientTimeout time.Duration, name string) (*ListenerServer, error) {
+	r := mux.NewRouter()
+
+	server := http.Server{
+		Handler:      r,
+		Addr:         listenAddr,
+		ReadTimeout:  clientTimeout,
+		WriteTimeout: clientTimeout,
+	}
+
+	listener, err := net.Listen("tcp", listenAddr)
+	if err != nil {
+		return nil, err
+	}
+	listenServer := ListenerServer{
+		name:     name,
+		listener: listener,
+	}
+
+	metricHandler := metricHandler{
+		metricCreationsMap: make(map[string]com_signalfx_metrics_protobuf.MetricType),
+	}
+	r.Handle("/v1/metric", &metricHandler)
+	r.Handle("/metric", &metricHandler)
+
+	listenServer.Keeper = stats.Combine(
+		setupNotFoundHandler(r, ctx, name),
+		setupProtobufV1(r, ctx, name, sink, &metricHandler),
+		setupJSONV1(r, ctx, name, sink, &metricHandler),
+		setupProtobufV2(r, ctx, name, sink),
+		setupJSONV2(r, ctx, name, sink),
+		setupCollectd(r, ctx, name, sink))
+
+	go server.Serve(listener)
+	return &listenServer, err
+}
+
+func setupNotFoundHandler(r *mux.Router, ctx context.Context, name string) stats.Keeper {
+	metricTracking := web.RequestCounter{}
+	r.NotFoundHandler = web.NewHandler(ctx, web.FromHTTP(http.NotFoundHandler())).Add(web.NextHTTP(metricTracking.ServeHTTP))
+	return stats.ToKeeperMany(map[string]string{"location": "listener", "name": name, "type": "http404"}, &metricTracking)
+}
+
+func setupChain(ctx context.Context, sink dpsink.Sink, name string, chainType string, getReader func(dpsink.Sink) ErrorReader) (*web.Handler, stats.Keeper) {
+	counter := &dpsink.Counter{}
+	finalSink := dpsink.FromChain(sink, dpsink.NextWrap(counter))
+	errReader := getReader(finalSink)
+	errorTracker := ErrorTrackerHandler{
+		reader: errReader,
+	}
+	metricTracking := web.RequestCounter{}
+	handler := web.NewHandler(ctx, &errorTracker).Add(web.NextHTTP(metricTracking.ServeHTTP))
+	st := stats.ToKeeperMany(protocol.ListenerDims(name, "sfx_"+chainType), &metricTracking, &errorTracker, counter)
+	return handler, st
+}
+
+func invalidContentType(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Invalid content type:"+r.Header.Get("Content-Type"), http.StatusBadRequest)
+}
+
+func setupProtobufV1(r *mux.Router, ctx context.Context, name string, sink dpsink.Sink, typeGetter MericTypeGetter) stats.Keeper {
+	handler, st := setupChain(ctx, sink, name, "protobuf_v1", func(s dpsink.Sink) ErrorReader {
+		return &ProtobufDecoderV1{Sink: s, TypeGetter: typeGetter}
+	})
+
+	SetupProtobufV1Paths(r, handler)
+	return st
+}
+
+// SetupProtobufV1Paths routes to R paths that should handle V1 Protobuf datapoints
+func SetupProtobufV1Paths(r *mux.Router, handler http.Handler) {
+	r.Path("/datapoint").Methods("POST").Headers("Content-Type", "application/x-protobuf").Handler(handler)
+	r.Path("/v1/datapoint").Methods("POST").Headers("Content-Type", "application/x-protobuf").Handler(handler)
+}
+
+func setupJSONV1(r *mux.Router, ctx context.Context, name string, sink dpsink.Sink, typeGetter MericTypeGetter) stats.Keeper {
+	handler, st := setupChain(ctx, sink, name, "json_v1", func(s dpsink.Sink) ErrorReader {
+		return &JSONDecoderV1{Sink: s, TypeGetter: typeGetter}
+	})
+
+	SetupJSONV1Paths(r, handler)
+	return st
+}
+
+// SetupJSONV1Paths routes to R paths that should handle V1 JSON datapoints
+func SetupJSONV1Paths(r *mux.Router, handler http.Handler) {
+	r.Path("/datapoint").Methods("POST").Headers("Content-Type", "application/json").Handler(handler)
+	r.Path("/v1/datapoint").Methods("POST").Headers("Content-Type", "application/json").Handler(handler)
+	r.Path("/datapoint").Methods("POST").Headers("Content-Type", "").HandlerFunc(invalidContentType)
+	r.Path("/v1/datapoint").Methods("POST").Headers("Content-Type", "").HandlerFunc(invalidContentType)
+	r.Path("/datapoint").Methods("POST").Handler(handler)
+	r.Path("/v1/datapoint").Methods("POST").Handler(handler)
+}
+
+func setupProtobufV2(r *mux.Router, ctx context.Context, name string, sink dpsink.Sink) stats.Keeper {
+	handler, st := setupChain(ctx, sink, name, "protobuf_v2", func(s dpsink.Sink) ErrorReader {
+		return &ProtobufDecoderV2{Sink: s}
+	})
+	SetupProtobufV2Paths(r, handler)
+
+	return st
+}
+
+// SetupProtobufV2Paths tells the router which paths the given handler (which should handle v2 protobufs)
+// should see
+func SetupProtobufV2Paths(r *mux.Router, handler http.Handler) {
+	r.Path("/v2/datapoint").Methods("POST").Headers("Content-Type", "application/x-protobuf").Handler(handler)
+}
+
+func setupJSONV2(r *mux.Router, ctx context.Context, name string, sink dpsink.Sink) stats.Keeper {
+	handler, st := setupChain(ctx, sink, name, "json_v2", func(s dpsink.Sink) ErrorReader {
+		return &JSONDecoderV2{Sink: s}
+	})
+	SetupJSONV2Paths(r, handler)
+	return st
+}
+
+// SetupJSONV2Paths tells the router which paths the given handler (which should handle v2 JSON)
+// should see
+func SetupJSONV2Paths(r *mux.Router, handler http.Handler) {
+	r.Path("/v2/datapoint").Methods("POST").Headers("Content-Type", "application/json").Handler(handler)
+	r.Path("/v2/datapoint").Methods("POST").Headers("Content-Type", "").HandlerFunc(invalidContentType)
+	r.Path("/v2/datapoint").Methods("POST").Handler(handler)
+}
+
+func setupCollectd(r *mux.Router, ctx context.Context, name string, sink dpsink.Sink) stats.Keeper {
+	h, st := collectd.SetupHandler(ctx, name, sink)
+	SetupCollectdPaths(r, h)
+	return st
+}
+
+// SetupCollectdPaths tells the router which paths the given handler (which should handle collectd json)
+// should see
+func SetupCollectdPaths(r *mux.Router, handler http.Handler) {
+	r.Path("/v1/collectd").Methods("POST").Headers("Content-Type", "application/json").Handler(handler)
+	r.Path("/v1/collectd").Methods("POST").Headers("Content-Type", "").HandlerFunc(invalidContentType)
+}
